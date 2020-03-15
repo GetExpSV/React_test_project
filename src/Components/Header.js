3e56c2f9216f6ab636e4d0499fa99470aee0251e
GetExpSV <savinykhvladislav@gamil.com>
2020-03-15 18:51:28 +0200

    begin project

---
+++ src/Components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () =>{
+    return (<header className="header">
+        <img src="https://thelogo.shop/wp-content/uploads/2016/12/09-Letter-B.png"/>
+    </header>);
+}
+
+export default Header;
---
+++ src/Components/Profile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Profile.css';
+
+const Profile = () => {
+    return (<div className="content">
+        <div>
+            <img
+                src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg"/>
+        </div>
+        <div>
+            <img src="https://interactive-examples.mdn.mozilla.net/media/examples/grapefruit-slice-332-332.jpg"/>
+        </div>
+    </div>);
+}
+
+export default Profile;
