6d5e0c15f15407150195909b18e8381f6bbe6cc9
GetExpSV <savinykhvladislav@gamil.com>
2020-03-16 17:00:06 +0200

    add NavLink into Navbar.js

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import nav_class from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (<nav className={nav_class.nav}>
-        <div className={nav_class.item}><a href="/profile">Profile</a></div>
-        <div className={nav_class.item}><a href="/messages">Messages</a></div>
-        <div className={nav_class.item}><a href="/news">News</a></div>
-        <div className={nav_class.item}><a href="/music">Music</a></div>
-        <div className={nav_class.item}><a href="/settings">Settings</a></div>
+        <div className={nav_class.item}>
+            <NavLink to="/profile" activeClassName={nav_class.activeLink}>Profile</NavLink>
+        </div>
+        <div className={nav_class.item}>
+            <NavLink to="/messages" activeClassName={nav_class.activeLink}>Messages</NavLink>
+        </div>
+        <div className={nav_class.item}>
+            <NavLink to="/news" activeClassName={nav_class.activeLink}>News</NavLink>
+        </div>
+        <div className={nav_class.item}>
+            <NavLink to="/music" activeClassName={nav_class.activeLink}>Music</NavLink>
+        </div>
+        <div className={nav_class.item}>
+            <NavLink to="/settings" activeClassName={nav_class.activeLink}>Settings</NavLink>
+        </div>
     </nav>);
 }
 
