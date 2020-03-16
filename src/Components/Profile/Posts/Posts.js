8ce9edd1de39f7f6f7174e3410ebfb8b1a8acac0
GetExpSV <savinykhvladislav@gamil.com>
2020-03-16 20:02:46 +0200

    change image

--- src/Components/Profile/Posts/Posts.js
+++ src/Components/Profile/Posts/Posts.js
@@ -3,7 +3,7 @@ import posts_class from './Posts.module.css';
 import Post from './Post/Post'
 
 const Posts = (props) => {
-    let image = "https://static.mk.ru/upload/entities/2019/05/08/00/articles/detailPicture/c7/b5/08/6e/5dda626cb409b1fa6942c29040609e17.jpg"
+    let image = "http://getdrawings.com/img/silhouette-avatar-12.png"
     return (<div className={posts_class.newPost}>New Post
         <div>
             <textarea></textarea>
