9cf903d510ae366dc8d96fc1092e5a9bf866a1d3
GetExpSV <savinykhvladislav@gamil.com>
2020-03-16 13:14:25 +0200

    use props into Post

--- src/Components/Profile/Posts/Post/Post.js
+++ src/Components/Profile/Posts/Post/Post.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import post_class from './Post.module.css';
 
-const Post = () => {
+const Post = (value) => {
     return(
         <div>
-            <div className={post_class.item}><img src="https://static.mk.ru/upload/entities/2019/05/08/00/articles/detailPicture/c7/b5/08/6e/5dda626cb409b1fa6942c29040609e17.jpg"/>
-            Post 1 <span>Like</span></div>
-            <div className={post_class.item}><img src="https://static.mk.ru/upload/entities/2019/05/08/00/articles/detailPicture/c7/b5/08/6e/5dda626cb409b1fa6942c29040609e17.jpg"/>
-            Post 2 <span>Like</span></div>
+            <div className={post_class.item}><img src={value.image}/>
+            {value.message} <span>Like {value.likeCount}</span></div>
         </div>
     );
 }
