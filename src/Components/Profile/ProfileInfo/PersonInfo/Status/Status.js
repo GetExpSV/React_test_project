cb5f022dd942da38d13b141244435e1b845aecb5
GetExpSV <savinykhvladislav@gamil.com>
2020-04-20 17:01:56 +0300

    add useEffect

--- src/Components/Profile/ProfileInfo/PersonInfo/Status/Status.js
+++ src/Components/Profile/ProfileInfo/PersonInfo/Status/Status.js
@@ -1,10 +1,14 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 
 let Status = (props) =>{
 
     let [editMod, setEditMod] = useState(false)
     let [status, setStatus] = useState(props.status)
+    
+    useEffect(()=>{
+        setStatus(props.status);
+    }, [props.status])
 
     let activateEditMod = () =>{
         setEditMod(true)
